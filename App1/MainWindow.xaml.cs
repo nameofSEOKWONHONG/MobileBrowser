b9fa905f4903e13bda5d36738e2cd2c74a8c5edb
SeokWon Hong <h20913@gmail.com>
2022-08-07 17:16:43 +0900

    create project

---
+++ App1/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using SettingsUI.Helpers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace App1
+{
+    /*
+     * TODO LIST
+     * 0. User setting start url.
+     * 1. Screen Capture.
+     * 2. Hide/Show url input & button.
+     * 3. Share capture to sns, messenger.
+     * 4. Share url to friends.
+     */
+    
+
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+            // C# code to set AppTitleBar uielement as titlebar            
+            this.ExtendsContentIntoTitleBar = true;  // enable custom titlebar
+            this.SetTitleBar(AppTitleBar);      // set user ui element as titlebar            
+        }
+    }
+
+
+}
